ed03f7197677179f7766c3b30c73a4960afc93c9
Cassindi <tara.devin.b@gmail.com>
2018-08-22 16:59:39 -0500

    not working yet

--- public/assets/js/wings.js
+++ public/assets/js/wings.js
@@ -1,61 +1,55 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    $(".change-sleep").on("click", function(event) {
-      var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
-  
-      var newSleepState = {
-        sleepy: newSleep
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
-        type: "PUT",
-        data: newSleepState
-      }).then(
-        function() {
-          console.log("changed sleep to", newSleep);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/cats", {
-        type: "POST",
-        data: newCat
-      }).then(
-        function() {
-          console.log("created new cat");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".delete-cat").on("click", function(event) {
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
-      $.ajax("/api/cats/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted cat", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+$("#add-wing").on("submit", function(event) {
+    event.preventDefault();
+
+    var wing = {
+      name: $("#wing-sauce").val().trim()
+    };
+    
+    // Send the POST request.
+    $.ajax("/api/wings", {
+      type: "POST",
+      data: wing
+    }).then(
+      function() {
+        console.log("added new wing");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
   });
-  
+
+  $("#eat").on("click", function(event) {
+    var id = $(this).data("id");
+
+    var newState = {
+      eaten: 1
+    };
+
+    // Send the DELETE request.
+    $.ajax("/api/wings/" + id, {
+      type: "PUT",
+      data: newState
+    }).then(
+      function() {
+        //console.log("deleted id ", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+
+ $("#delete").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/wings/" + id, {
+      type: "DELETE",
+    }).then(
+      function() {
+        console.log("deleted wing", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
